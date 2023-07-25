8c97cb4898f01735458bf776b0646f126b8bbb38
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-07-25 18:11:24 +0200

    projects links

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -152,10 +152,7 @@ export default function Carrousel({ images }) {
               <p>{selectedProject.description}</p>
             </div>
             <div className="project-links">
-              <a
-                href="https://github.com/ClementBartholome/P6-OpenClassrooms"
-                target="_blank"
-                rel="noreferrer">
+              <a href={selectedProject.code} target="_blank" rel="noreferrer">
                 Code
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -166,10 +163,7 @@ export default function Carrousel({ images }) {
                   <path d="M12 .5C5.4.5.5 5.4.5 12c0 5.3 3.4 9.8 8.2 11.4.6.1.8-.2.8-.6v-2c-3.3.7-4-1.5-4-1.5-.6-1.5-1.4-1.9-1.4-1.9-1.1-.7.1-.7.1-.7 1.2.1 1.9 1.2 1.9 1.2 1.1 1.9 2.8 1.3 3.5 1 .1-.8.4-1.3.8-1.6-3.1-.4-6.4-1.6-6.4-7.2 0-1.6.6-2.9 1.5-3.9-.2-.4-.7-1.8.2-3.7 0 0 1.2-.4 3.9 1.4 1.1-.3 2.3-.4 3.5-.4 1.2 0 2.4.2 3.5.4 2.7-1.8 3.9-1.4 3.9-1.4.9 1.9.3 3.3.2 3.7.9 1 1.5 2.3 1.5 3.9 0 5.6-3.3 6.8-6.4 7.2.5.4.9 1.1.9 2.2v3.2c0 .4.3.8.8.6C20.6 21.8 24 17.3 24 12c0-6.6-5-11.5-11.5-11.5z" />
                 </svg>
               </a>
-              <a
-                href="https://clementbartholome.github.io/P6-OpenClassrooms"
-                target="_blank"
-                rel="noreferrer">
+              <a href={selectedProject.demo} target="_blank" rel="noreferrer">
                 Démo
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
