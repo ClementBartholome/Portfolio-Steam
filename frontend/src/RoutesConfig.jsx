229231e9ff217a63c8e5f77ecbd924bd94825f94
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-07-31 18:08:27 +0200

    fix RoutesConfig name

--- frontend/src/RoutesConfig.jsx
+++ frontend/src/RoutesConfig.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
-export default function App() {
+export default function RoutesConfig() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
