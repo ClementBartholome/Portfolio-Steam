442040a49d49b8773f303dbc2ba0efa29bd46e0d
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-07-31 17:57:53 +0200

    test deploy

--- frontend/src/RoutesConfig.jsx
+++ frontend/src/RoutesConfig.jsx
@@ -6,8 +6,8 @@ import LoginPage from "./pages/LoginPage";
 export default function App() {
   return (
     <Routes>
-      <Route path="/Portfolio-Steam" element={<HomePage />} />
-      <Route path="/Portfolio-Steam/login" element={<LoginPage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
